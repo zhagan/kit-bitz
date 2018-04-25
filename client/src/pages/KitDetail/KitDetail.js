13c695ec57b2cd4c3586c5d277e624f48f61e719
Dean Hooker <deanhooker91@gmail.com>
2018-04-24 19:20:56 -0600

    bom to user inv comparison table

--- client/src/pages/KitDetail/KitDetail.js
+++ client/src/pages/KitDetail/KitDetail.js
@@ -84,68 +84,92 @@ class Detail extends Component {
     return (
       <CenterContainer>
 
-      <Row>
-      <Col size="md-12">
-        <div className="details-center">
-        <div className="details-title">
-          <h1>{this.state.kit.kitName} </h1>
-          
-          <h5>Created by <span id="pop-blue">{this.state.kit.createdBy.username}</span></h5>
-          </div>
+        <Row>
+          <Col size="md-12">
+            <div className="details-center">
+              <div className="details-title">
+                <h1>{this.state.kit.kitName} </h1>
 
-          <div className="details-image-container">
-                        <img
-                        src={cardImage}
-                        alt="Kit-Bitz"
-                        />
-          </div>
-      
-          <div className="details-content">
-            
-              <h5>This Kit is designed by <span id="pop-blue">{this.state.kit.designer}</span></h5>
-              <p>
-              <a href={this.state.kit.kitUrl} target="_blank">Kit Link</a><br />
-              <a href={this.state.kit.pcbUrl} target="_blank">PCB Link</a><br />
-              <a href={this.state.kit.faceplateUrl} target="_blank">Faceplate Link</a>
-              <br />
-            </p>
-            <h5>
-            <Button
-            onClick={this.compareInventory}
-            > Compare to Inventory
-            </Button>
-            </h5>
-          </div>
-          <div className="details-summary">
-            <p><small>A power switch using a push button. It turns on with a single press and only turns off when you hold the button down. This could be used to get functionality similar to most laptops where a single press when on will initiate a "soft" shutdown but you can force a "hard" shutdown by holding it down.
+                <h5>Created by <span id="pop-blue">{this.state.kit.createdBy.username}</span></h5>
+              </div>
 
-            The circuit is from this article.
-            The board is my own design and features:
+              <div className="details-image-container">
+                <img
+                  src={cardImage}
+                  alt="Kit-Bitz"
+                />
+              </div>
+
+              <div className="details-content">
+
+                <h5>This Kit is designed by <span id="pop-blue">{this.state.kit.designer}</span></h5>
+                <p>
+                  <a href={this.state.kit.kitUrl} target="_blank">Kit Link</a><br />
+                  <a href={this.state.kit.pcbUrl} target="_blank">PCB Link</a><br />
+                  <a href={this.state.kit.faceplateUrl} target="_blank">Faceplate Link</a>
+                  <br />
+                </p>
+                <h5>
+                  <Button
+                    onClick={this.compareInventory}
+                  > Compare to Inventory
+                  </Button>
+                </h5>
+              </div>
+              <div className="details-summary">
+                <p><small>A power switch using a push button. It turns on with a single press and only turns off when you hold the button down. This could be used to get functionality similar to most laptops where a single press when on will initiate a "soft" shutdown but you can force a "hard" shutdown by holding it down.
+
+                The circuit is from this article.
+                The board is my own design and features:
             </small></p>
-              <ul><small>
-                <li>Standard 0.1" spacing, fits neatly onto a breadboard</li>
-                <li>On-board button (and pins to connect your own)</li>
-                <li>Can switch 3-20V and up to 4A</li>
-                <li>Two LEDs indicate input and output power</li>
-            </small></ul>
-             
-          </div>
-      
-     
-        
-          
+                <ul><small>
+                  <li>Standard 0.1" spacing, fits neatly onto a breadboard</li>
+                  <li>On-board button (and pins to connect your own)</li>
+                  <li>Can switch 3-20V and up to 4A</li>
+                  <li>Two LEDs indicate input and output power</li>
+                </small></ul>
 
-          </div>
-      </Col>
-      </Row>
-      
+              </div>
+            </div>
+            <h3>Parts I have in this kit</h3>
+            {this.state.bomComparisonArray.length ? (
+              <div id='comparisonTable'>
+                <div className="table-responsive">
+                  <table className="table table-striped table-bordered table-hover">
+                    <thead>
+                      <tr>
+                        <th>MPN</th>
+                        <th>Qty</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {
+                        this.state.bomComparisonArray.map(item => {
+                          return (
+                            <tr>
+                              <td>{item.MPN}</td>
+                              <td>{item.Qty}</td>
+                            </tr>
+                          )
+                        })
+                      }
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            ) : (
+                <h3>No Results to Display</h3>
+              )}
+          </Col>
+        </Row>
 
-      <Row>
-        <Col size="md-4">
-          <Link to="/createKit">← Back to Create Kit</Link>
-        </Col>
-      </Row>
-    </CenterContainer>
+
+        <Row>
+          <Col size="md-4">
+            <Link to="/createKit">← Back to Create Kit</Link>
+          </Col>
+        </Row>
+      </CenterContainer>
     );
   }
 }
