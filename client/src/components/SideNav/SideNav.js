c59000f32b874b81f6cd4a5308b6b587d8294af9
Amy Lyne <amy.lyne92@gmail.com>
2018-04-27 13:54:30 -0600

    sidenav and card pop and flare

--- client/src/components/SideNav/SideNav.js
+++ client/src/components/SideNav/SideNav.js
@@ -35,29 +35,30 @@ itemSelected = id =>{
 render () {
 
     return (
-
-        <Nav bsStyle="pills" stacked activeKey={1} onSelect={this.itemSelected}>
-            <LinkContainer to="/parts">
-            <NavItem eventKey={1} id='parts'>
-            <SvgIcon style={{ verticalAlign: 'middle' }} size={30} icon={ic_search}/>&nbsp;&nbsp;Parts
-            </NavItem>
-            </LinkContainer>
-            <LinkContainer to="/inventory">
-            <NavItem eventKey={2} id='inventory' title="Item">
-            <SvgIcon style={{ verticalAlign: 'middle' }} size={30} icon={ic_view_list}/>&nbsp;&nbsp;Inventory
-            </NavItem>
-            </LinkContainer>
-            <LinkContainer to="/kits">
-            <NavItem eventKey={3} id='kits' title="Item">
-            <SvgIcon style={{ verticalAlign: 'middle' }} size={30} icon={circuitBoard}/>&nbsp;&nbsp;Kits
-            </NavItem>
-            </LinkContainer>
-            <LinkContainer to="/createkit">
-            <NavItem eventKey={4} id='createkit' title="Item">
-            <SvgIcon style={{ verticalAlign: 'middle' }} size={30} icon={plusCircle}/>&nbsp;&nbsp;Create Kits
-            </NavItem>
-            </LinkContainer>
-        </Nav>
+        <div className = "sidenav-container">
+            <Nav bsStyle="pills" stacked activeKey={1} onSelect={this.itemSelected}>
+                <LinkContainer to="/parts">
+                <NavItem eventKey={1} id='parts'>
+                <SvgIcon style={{ verticalAlign: 'middle' }} size={30} icon={ic_search}/>&nbsp;&nbsp;Parts
+                </NavItem>
+                </LinkContainer>
+                <LinkContainer to="/inventory">
+                <NavItem eventKey={2} id='inventory' title="Item">
+                <SvgIcon style={{ verticalAlign: 'middle' }} size={30} icon={ic_view_list}/>&nbsp;&nbsp;Inventory
+                </NavItem>
+                </LinkContainer>
+                <LinkContainer to="/kits">
+                <NavItem eventKey={3} id='kits' title="Item">
+                <SvgIcon style={{ verticalAlign: 'middle' }} size={30} icon={circuitBoard}/>&nbsp;&nbsp;Kits
+                </NavItem>
+                </LinkContainer>
+                <LinkContainer to="/createkit">
+                <NavItem eventKey={4} id='createkit' title="Item">
+                <SvgIcon style={{ verticalAlign: 'middle' }} size={30} icon={plusCircle}/>&nbsp;&nbsp;Create Kits
+                </NavItem>
+                </LinkContainer>
+            </Nav>
+        </div>
 
 
 )
